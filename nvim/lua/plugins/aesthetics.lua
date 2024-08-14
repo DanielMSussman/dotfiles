3e5cd5cad3da5b153ad1a70b805a94d666cd151b
Daniel Sussman <daniel.m.sussman@emory.edu>
2024-08-14 13:50:47 -0400

    relative path on lualine

--- nvim/lua/plugins/aesthetics.lua
+++ nvim/lua/plugins/aesthetics.lua
@@ -1,29 +1,35 @@
 return {
     --a few color schemes... the default one should apparently get high priority
     {
-        "sho-87/kanagawa-paper.nvim",
-        priority=1000,
-    },
-    {
         "rebelot/kanagawa.nvim",
         priority=1000,
         config = function()
             vim.cmd.colorscheme "kanagawa"
         end
     },
-    --light-themed solarized, for old time's sake
-    {
-        'shaunsingh/solarized.nvim',
-        priority=1000,
-    },
 
     --lualine
     {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-            require("lualine").setup()
-    end
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require("lualine").setup({
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {{
+                        'filename',
+                        newfile_status = true,  -- Display new file status (new file means no write after created)
+                        path = 1,                -- 0: Just the filename
+
+                        shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+                                }},
+                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },    
+            })
+        end
     },
 
 
