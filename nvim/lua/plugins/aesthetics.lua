0f8ca2724bf293c8bf02d69a32958d213d358963
Daniel Sussman <daniel.m.sussman@emory.edu>
2024-08-16 16:28:02 -0400

    nudge

--- nvim/lua/plugins/aesthetics.lua
+++ nvim/lua/plugins/aesthetics.lua
@@ -24,9 +24,9 @@ return {
 
                         shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
                                 }},
-                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_x = { 'fileformat', 'filetype'},
                     lualine_y = {'progress'},
-                    lualine_z = {'location'}
+                    lualine_z = {'location'},
                 },    
             })
         end
