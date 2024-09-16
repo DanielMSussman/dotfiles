efb9b227ab9ec411b40e4636c8d4c28d54d1f716
Daniel M Sussman <dmsussman@gmail.com>
2024-09-16 08:25:48 -0400

    get rid of wezterm opacity (windows visual artifacts)

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,7 +13,7 @@ config.font = wezterm.font {
         harfbuzz_features = {'calt=0','clig=0','liga=0'},
     }
 config.font_size = 13
-config.window_background_opacity =1
+--config.window_background_opacity =1
 config.window_decorations = "TITLE | RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
