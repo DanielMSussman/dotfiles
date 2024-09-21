d8b50b26dacd6e9c4cc8eb91f811933e2d29627e
Daniel <dmsussman@gmail.com>
2024-09-21 13:09:10 -0400

    div grad curl snippets

---
+++ nvim/lua/luasnip/tex/mathSymbols.lua
@@ -0,0 +1,55 @@
+-- use vimtex to determine if we are in a math context
+local function math()
+    return vim.api.nvim_eval('vimtex#syntax#in_mathzone()') == 1
+end 
+
+return {
+
+    s({trig=";O", snippetType="autosnippet", desc="mathcal O",wordTrig=false},
+        {
+            t("\\mathcal{O}"),
+        }
+    ),
+    s({trig=";i", snippetType="autosnippet", desc="infinity",wordTrig=false},
+        {
+            t("\\infty"),
+        }
+    ),
+    s({trig=";N", snippetType="autosnippet", desc="nabla",wordTrig=false},
+        {
+            t("\\nabla"),
+        }
+    ),
+    s({trig="div", snippetType="autosnippet", desc="nabla",wordTrig=false},
+        {
+            t("\\nabla\\cdot"),
+        },
+        { condition=math }
+    ),
+    s({trig="grad", snippetType="autosnippet", desc="gradient",wordTrig=false},
+        {
+            t("\\nabla"),
+        },
+        { condition=math }
+    ),
+    s({trig="curl", snippetType="autosnippet", desc="curl",wordTrig=false},
+        {
+            t("\\nabla\\times"),
+        },
+        { condition=math }
+    ),
+    --postfixes for vectors, hats, etc. The match pattern is '\\' plus the default (so that hats get put on greek letters, etc)
+    postfix({trig="hat", match_pattern = [[[\\%w%.%_%-%"%']+$]], snippetType="autosnippet",dscr="postfix hat when in math mode"},
+        {l("\\hat{" .. l.POSTFIX_MATCH .. "}")}, 
+        { condition=math }
+    ) ,
+    postfix({trig="vec", match_pattern = [[[\\%w%.%_%-%"%']+$]] ,snippetType="autosnippet",dscr="postfix vec when in math mode"},
+        {l("\\vec{" .. l.POSTFIX_MATCH .. "}")}, 
+        { condition=math }
+    ) ,
+
+    postfix({trig="df",snippetType="autosnippet",desc="postfix differential (physics package)"},
+        {l("\\dd{" .. l.POSTFIX_MATCH .. "}")}, 
+        {condition = math}
+    ),
+}
