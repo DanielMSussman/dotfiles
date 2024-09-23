a1ec0ba39b7f74b1d21e58f5c3d0c0086d89c4f1
Daniel M Sussman <dmsussman@gmail.com>
2024-09-23 09:08:40 -0400

    all luasnip for demo

--- nvim/lua/luasnip/all.lua
+++ nvim/lua/luasnip/all.lua
@@ -1,3 +1,8 @@
 --a sample snippet that will work within all filetypes
 return {
+    s({trig="helloworld", snippetType="snippet", desc="alpha",wordTrig=false},
+        {
+            t("Just a hint of what's to come!"),
+        }
+    ),
 }
