8493e6b131aee8c739dc1803df0c41468919e862
Daniel <dmsussman@gmail.com>
2024-09-27 10:44:42 -0400

    latex word snippet

--- nvim/lua/luasnip/all.lua
+++ nvim/lua/luasnip/all.lua
@@ -1,3 +1,8 @@
 --a sample snippet that will work within all filetypes
 return {
+    s({trig="latex", snippetType="snippet", desc="LaTeX"},
+        {
+            t("LaTeX"),
+        }
+    ),
 }
