cc8e45984071f43f63a2dd8d04e99b2b3fe26a19
Daniel M Sussman <dmsussman@gmail.com>
2024-11-04 08:54:08 -0500

    stop obsidian checkbox toggling on return

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -46,6 +46,32 @@ return{
                     },
                 },
             },
+            mappings = {
+                -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+                ["gf"] = {
+                    action = function()
+                        return require("obsidian").util.gf_passthrough()
+                    end,
+                    opts = { noremap = false, expr = true, buffer = true },
+                },
+                -- Toggle check-boxes.
+                ["<leader>ch"] = {
+                    action = function()
+                        return require("obsidian").util.toggle_checkbox()
+                    end,
+                    opts = { buffer = true },
+                },
+                -- Smart action depending on context, either follow link or toggle checkbox.
+                ["<cr>"] = {
+                    action = function()
+                        if require("obsidian").util.cursor_on_markdown_link(nil,nil,true) then
+                            return "<cmd>ObsidianFollowLink<cr>"
+                        end
+                        -- return require("obsidian").util.smart_action()
+                    end,
+                    opts = { buffer = true, expr = true },
+    }
+  },
             disable_frontmatter = false,
             notes_subdir = "notes",
             new_notes_location = "notes_subdir",
