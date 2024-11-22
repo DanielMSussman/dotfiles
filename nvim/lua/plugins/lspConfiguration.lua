c42c7a6dbfe552e3c29621b735ac767106d34556
Daniel M Sussman <dmsussman@gmail.com>
2024-11-22 09:42:09 -0500

    lsp nudges

--- nvim/lua/plugins/lspConfiguration.lua
+++ nvim/lua/plugins/lspConfiguration.lua
@@ -1,17 +1,20 @@
 return {
     {
     "williamboman/mason.nvim",
-    event = "VeryLazy",
+    -- event = "VeryLazy",
+    lazy=true,
     config = function()
         require("mason").setup()
     end
     },
     {
     "williamboman/mason-lspconfig.nvim",
-    event = "VeryLazy",
+    -- event = "VeryLazy",
+    lazy=true,
     config = function()
         require("mason-lspconfig").setup({
             -- ensure_installed = {"clangd"}
+                -- ensure_installed = {"ts_ls"}
             }
             )
     end
@@ -24,6 +27,8 @@ return {
             --advertise cmp-nvim-lsp
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
         lspconfig.clangd.setup {capabilities = capabilities,
+            autostart=false,}
+        lspconfig.ts_ls.setup {capabilities = capabilities,
             autostart=false,}
     end
     },
