5fa01c073b975c2bc7747640c8bb49f5f55296ff
Daniel M Sussman <dmsussman@gmail.com>
2024-11-24 11:48:51 -0500

    nudge to clang-format options in formatter

--- nvim/lua/plugins/formatAndLinting.lua
+++ nvim/lua/plugins/formatAndLinting.lua
@@ -14,7 +14,7 @@ return {
             formatters = {
                 cppFormatter= {
                         command = "clang-format",
-                        args = '-style="{BasedOnStyle: WebKit, AccessModifierOffset: 0,AlignAfterOpenBracket: Align, AlignOperands: Align, AllowShortIfStatementsOnASingleLine: Never, AllowShortLoopsOnASingleLine: false, BinPackArguments: false, BinPackParameters: false, BreakBeforeBraces: Whitesmiths, BreakConstructorInitializers: BeforeColon, Cpp11BracedListStyle: true, FixNamespaceComments: true, IndentWidth: 4, Language: Cpp, PointerAlignment: Left, SpaceAfterTemplateKeyword: false, SpaceBeforeParens: ControlStatements, Standard: c++14, TabWidth: 4, UseTab: Never, IndentCaseBlocks: true, AllowShortCaseLabelsOnASingleLine: true}"',
+                        args = '-style="{BasedOnStyle: WebKit, AccessModifierOffset: 0,AlignAfterOpenBracket: Align, AlignOperands: Align, AllowShortIfStatementsOnASingleLine: Never, AllowShortLoopsOnASingleLine: false, BinPackArguments: false, BinPackParameters: false, BreakBeforeBraces: Whitesmiths, BreakConstructorInitializers: BeforeColon, Cpp11BracedListStyle: true, FixNamespaceComments: true, IndentWidth: 4, Language: Cpp, PointerAlignment: Left, SpaceAfterTemplateKeyword: false, SpaceBeforeParens: ControlStatements, Standard: c++14, TabWidth: 4, UseTab: Never, IndentCaseLabels: true, AllowShortCaseLabelsOnASingleLine: true}"',
                         stdin = true,
                     },
                 texFormatter= {
