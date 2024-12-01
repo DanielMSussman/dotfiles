f79f68a34c8f90c9a02a38859cae7b3bf8e9840c
Daniel M Sussman <dmsussman@gmail.com>
2024-12-01 14:12:54 -0500

    refined class member implementation api command

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -1,33 +1,50 @@
-local function functionDeclarationExistsInFile(cpp_filename, functionDeclarationToTest)
+local function appendSignatureToTable(contentToAppend,signature)
+    table.insert(contentToAppend,"")
+    table.insert(contentToAppend, signature)
+    table.insert(contentToAppend, "    {")
+    table.insert(contentToAppend, "    }")
+end
+local function fileIsOpenInBuffer(filename)
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_name(buf) == filename then
+      return true
+    end
+  end
+  return false
+end
+local function functionDeclarationExistsInFile(cppFilename, functionDeclarationToTest)
     -- Read the file content, concatenate into a single string, and parse it
-    local file_content = vim.fn.readfile(cpp_filename)
-    local fileString = table.concat(file_content)
+    local fileContent = vim.fn.readfile(cppFilename)
+    local fileString = table.concat(fileContent)
 
-    local parser = vim.treesitter.get_string_parser(table.concat(file_content),"cpp")
+    local parser = vim.treesitter.get_string_parser(table.concat(fileContent),"cpp")
     local tree = parser:parse()
-    local root_node = tree[1]:root()
-    --pattern to look for is declaration -> init_declarator -> function_declarator
-    for i = 0, root_node:named_child_count()-1, 1 do
-        local nextChild = root_node:named_child(i)
-        local nodeType = nextChild:type()
-        local nodeText = vim.treesitter.get_node_text(nextChild,0)
-        if nodeType == "declaration" then
-            for j = 0, nextChild:named_child_count()-1,1 do 
-                local subNode = nextChild:named_child(j)
-                nodeType = subNode:type()
-                nodeText = vim.treesitter.get_node_text(subNode,0)
-                if nodeType == "init_declarator" then
-                    for k = 0, subNode:named_child_count()-1,1 do 
-                        local functionNode = subNode:named_child(k)
-                        nodeType = functionNode:type()
-                        nodeText = vim.treesitter.get_node_text(functionNode,fileString)
-                        if nodeType == "function_declarator" then
-                            local functionDeclaration = vim.treesitter.get_node_text(functionNode,fileString)
-                            if functionDeclaration == functionDeclarationToTest then
-                                return true
-                            end
-                        end
-                    end
+    local rootNode = tree[1]:root()
+
+    --define a query for function_declarators
+    -- local query = vim.treesitter.query.parse(
+    --     "cpp",
+    --     [[
+    --   (declaration 
+    --     (init_declarator 
+    --       (function_declarator) @function_declarator))
+    -- ]]
+    -- )
+    local query = vim.treesitter.query.parse(
+        "cpp",
+        [[
+      (function_definition 
+          (function_declarator) @function_declarator)
+    ]]
+    )
+    -- Run the query on the parsed tree, and iterate over all matches
+    local matches = query:iter_matches(rootNode:root(), fileString)
+    for _, match in matches do
+        for id, node in pairs(match) do
+            if id == 1 then -- Check if it's the function_declarator node
+                local functionDeclaration = vim.treesitter.get_node_text(node, fileString)
+                if functionDeclaration == functionDeclarationToTest then
+                    return true
                 end
             end
         end
@@ -36,93 +53,86 @@ local function functionDeclarationExistsInFile(cpp_filename, functionDeclaration
     return false
 end
 
-function append_function_signature_to_cpp()
+local function appendAllImplementationsToCPP()
+    local currentFile = vim.fn.expand("%:p")
+    local cppFilename = currentFile:gsub("%.h$", ".cpp")
     local className = "myClass"
     local returnType = "void"
     local functionDeclaration = ""
+    local contentToAppend = {}
 
-  -- Get the current line's TreeSitter node
-    local current_node = vim.treesitter.get_node()
-    --ascend the tree until we find a field_declaration
-    while current_node do
-        if tostring(current_node:type()) == 'field_declaration' then
-            break
-        end
-        current_node =current_node:parent()
+    -- if the file is not readable, include the header on the first line
+    local fileIsReadable = vim.fn.filereadable(cppFilename)
+    -- if the file doesn't exist, include the header
+    if fileIsReadable == 0  then
+        -- MiniNotify.add("file not read")
+        table.insert(contentToAppend,"#include \"" ..vim.fn.expand("%").."\"")
     end
 
-    if not current_node then
-        MiniNotify.add('Not inside a function declaration','INFO')
+    -- get the class specifier we're sitting inside of
+    local currentNode = vim.treesitter.get_node()
+    while currentNode do
+        if tostring(currentNode:type()) == 'class_specifier' then
+            break
+        end
+        currentNode =currentNode:parent()
+    end
+    local classNode = currentNode
+    if not classNode then
+        vim.notify('Not inside a class')
         return ""
     end
 
-
-    local fieldDeclaration = current_node
-
-    --get class name: ascend to the class specifier then find the type_identifier
-    while current_node do
-        if tostring(current_node:type()) == 'class_specifier' then
-            break
-        end
-        current_node =current_node:parent()
-    end
-    for i = 0, current_node:named_child_count()-1, 1 do
-        local childNode = current_node:named_child(i)
+    for i = 0, classNode:named_child_count()-1, 1 do
+        local childNode = classNode:named_child(i)
         if tostring(childNode:type()) == 'type_identifier' then
             className = vim.treesitter.get_node_text(childNode,0)
             break
         end
     end
-    --
-    -- MiniNotify.add(className,'WARN')
 
-    for i = 0, fieldDeclaration:named_child_count()-1, 1 do
-        local fieldChild = fieldDeclaration:named_child(i)
-        local nodeType = fieldChild:type()
-        if tostring(nodeType) == 'primitive_type' then
-            returnType = vim.treesitter.get_node_text(fieldChild,0)
-        end
-        if tostring(nodeType) == 'function_declarator' then
-            functionDeclaration = vim.treesitter.get_node_text(fieldChild,0)
+    -- from the class node, query for pairs of types and function declarations
+    local query = vim.treesitter.query.parse(
+        "cpp",
+        [[
+        (field_declaration
+          (primitive_type) @primitive_type
+          (function_declarator) @function_declarator)
+      ]]
+    )
+    local matches = query:iter_matches(classNode:root(), 0)
+    for _, match in matches do 
+        local primitiveNode = match[1]
+        local functionNode = match[2]
+
+        local primitiveTypeText = vim.treesitter.get_node_text(primitiveNode,0)
+        local functionDeclarationText = vim.treesitter.get_node_text(functionNode,0)
+        local signature = string.format(
+            "%s %s::%s",primitiveTypeText,className,functionDeclarationText)
+        if fileIsReadable == 0 then
+            vim.notify("adding "..functionDeclarationText.." to cpp file")
+            appendSignatureToTable(contentToAppend,signature)
+        end 
+        if fileIsReadable ~= 0 then
+            if functionDeclarationExistsInFile(cppFilename,tostring(className.."::"..functionDeclarationText)) then
+                vim.notify("implementation for "..functionDeclarationText.." already written")
+            else
+                vim.notify("adding "..functionDeclarationText.." to cpp file")
+                appendSignatureToTable(contentToAppend,signature)
+            end
         end
     end
-    -- MiniNotify.add(returnType,'WARN')
-    -- MiniNotify.add(functionDeclaration,'WARN')
-    local signature = string.format(
-        "%s %s::%s",returnType,className,functionDeclaration)
-    -- MiniNotify.add(signature)
-
-    local current_file = vim.fn.expand("%:p")
-    local cpp_filename = current_file:gsub("%.h$", ".cpp")
-
-    -- MiniNotify.add(current_file)
-    -- MiniNotify.add(cpp_filename)
-
-    local fileIsReadable = vim.fn.filereadable(cpp_filename)
-    local file_content = {}
-    -- if the file doesn't exist, include the header
-    if fileIsReadable == 0  then
-        -- MiniNotify.add("file not read")
-        table.insert(file_content,"#include \"" ..vim.fn.expand("%").."\"")
-    else
-        -- MiniNotify.add("file read")
-        local implementationAlreadyExists = functionDeclarationExistsInFile(cpp_filename,tostring(className.."::"..functionDeclaration))
-        if implementationAlreadyExists then
-            vim.notify("You've already written an implementation for that function")
-            return false
-        end
-
-    end
-
+    
     --append function implementation stub to the cpp file
-    table.insert(file_content,"")
-    table.insert(file_content, signature)
-    table.insert(file_content, "    {")
-    table.insert(file_content, "    };")
-    vim.fn.writefile(file_content,cpp_filename,"a")
-    -- vim.defer_fn(function() MiniNotify.clear() end, 10000)
-
+    vim.fn.writefile(contentToAppend,cppFilename,"a")
+    -- -- vim.defer_fn(function() MiniNotify.clear() end, 10000)
+    -- refresh if the buffer is open
+    if fileIsOpenInBuffer(cppFilename) then
+        vim.cmd("checktime "..cppFilename)
+    end
 end
 
-
-vim.keymap.set("n", "<localleader>i", ":lua append_function_signature_to_cpp()<CR>:checktime<CR>",{desc = '[I]mplement class member declaration'})
+vim.api.nvim_create_user_command('ImplementClassMemberFunctions', function()
+  appendAllImplementationsToCPP()
+end, { desc = 'Implement class member declarations' })
+vim.keymap.set("n", "<localleader>i", ":ImplementClassMemberFunction<CR>",{desc = '[I]mplement class member declarations'})
