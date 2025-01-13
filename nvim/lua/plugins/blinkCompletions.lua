d32c119383dfdbdce6b76f9d2ef53dc234da7c11
Daniel M Sussman <dmsussman@gmail.com>
2025-01-13 16:40:08 -0500

    0.10 updates to config for blink.cmp's interaction with luasnip

--- nvim/lua/plugins/blinkCompletions.lua
+++ nvim/lua/plugins/blinkCompletions.lua
@@ -16,7 +16,6 @@ return {
                 })
             end
     end,
-    vim.keymap.set("i", "<C-s>", function() require('cmp_vimtex.search').search_menu() end)
     },
     {
     'saghen/blink.compat',
@@ -36,7 +35,7 @@ return {
     lazy = false, -- lazy loading handled internally
 
     -- use a release tag to download pre-built binaries
-    version = 'v0.*',
+    version = '*',
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
@@ -47,56 +46,26 @@ return {
         -- see the "default configuration" section below for full documentation on how to define
         -- your own keymap.
         keymap = { preset = 'default' },
-
-        appearance = {
-            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-            -- Useful for when your theme doesn't support blink.cmp
-            -- will be removed in a future release
-            use_nvim_cmp_as_default = true,
-            -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- Adjusts spacing to ensure icons are aligned
-            nerd_font_variant = 'mono'
-        },
-
-        --luasnip setup
-        snippets = {
-            expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-            active = function(filter)
-                if filter and filter.direction then
-                    return require('luasnip').jumpable(filter.direction)
-                end
-                return require('luasnip').in_snippet()
-            end,
-            jump = function(direction) require('luasnip').jump(direction) end,
-        },
-        -- default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, via `opts_extend`
-        sources = {
-            -- default = { 'lsp', 'path', 'luasnip','buffer' },
-            completion = {
-                enabled_providers = {'lsp', 'path', 'luasnip', 'buffer','vimtex'},
-                },
-            -- optionally disable cmdline completions
-            -- cmdline = {},
-            providers = {
-                luasnip = {
-                    score_offset = 1,
+            snippets = { preset = 'luasnip' },
+            -- ensure you have the `snippets` source (enabled by default)
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer','vimtex' },
+                providers = {
+                    snippets = {
+                        score_offset = 1,
                     },
-                -- create provider
-                vimtex = {
-                    name = 'vimtex',
-                    module = 'blink.compat.source',
-                    score_offset = 3,
+                    vimtex = {
+                        name = 'vimtex',
+                        module = 'blink.compat.source',
+                        score_offset = 3,
                     },
                 },
             },
-        -- experimental signature help support
-        signature = { enabled = true },
+            -- experimental signature help support
+            signature = { enabled = true },
         },
-
-
-    -- allows extending the providers array elsewhere in your config
-    -- without having to redefine it
-    opts_extend = { "sources.default" },
+        -- allows extending the providers array elsewhere in your config
+        -- without having to redefine it
+        opts_extend = { "sources.default" },
     },
 }
