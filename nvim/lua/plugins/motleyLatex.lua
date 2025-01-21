641238a558940f0ccd3645d9e0da42de1ba03551
Daniel M Sussman <dmsussman@gmail.com>
2025-01-21 08:31:00 -0500

    motleyLatex from repo

--- nvim/lua/plugins/motleyLatex.lua
+++ nvim/lua/plugins/motleyLatex.lua
@@ -1,19 +1,18 @@
 return {
     {
-    dir = '~/repos/motleyLatex.nvim',
-    -- "DanielMSussman/motleyLatex.nvim",
+    -- dir = '~/repos/motleyLatex.nvim',
+    "DanielMSussman/motleyLatex.nvim",
     config = function()
         require("motleyLatex").setup({
                     tcolorbox_opts = {
+                    --overwrite any default options, or add new ones
                         colframe = "black!40",
-                        boxrule = "1.5pt", --overwrite any default options, or add new ones
+                        boxrule = "0.5pt", 
                         width = "0.9\\textwidth",
+                        fontupper = "\\footnotesize",
+                        
                     },
         })
-        -- vim.keymap.set("n", "<localleader>c", function() vim.cmd("ImplementMembersInClass") end,{desc = 'implement [c]lass member declarations'})
-        -- vim.keymap.set("n", "<localleader>l", function() vim.cmd("ImplementMemberOnCursorLine") end,{desc = 'implement member current [l]ine'})
-        -- vim.keymap.set("n", "<localleader>s", function() vim.cmd("StPatrick") end,{desc = 'drive out the [s]nakes'})
-        -- vim.keymap.set("n", "<localleader>d", function() vim.cmd("CreateDerivedClass") end,{desc = 'Create a class which [d]erives from the current one'})
     end
     },
 }
