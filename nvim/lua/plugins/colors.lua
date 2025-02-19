519d527898a845146bbeab3e7688f3cbd07f7d91
Daniel M Sussman <dmsussman@gmail.com>
2025-02-19 08:20:57 -0500

    update packages

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,26 +1,6 @@
 --For colorschemes that I am not using right now
 return {
     {
-        "sho-87/kanagawa-paper.nvim",
-        lazy=true,
-        -- lazy = false,
-        -- priority = 1000,
-        config  =  function()
-            require('kanagawa-paper').setup({
-                undercurl = false,
-                transparent = false,
-                gutter = false,
-                dimInactive = true, -- disabled when transparent
-                terminalColors = true,
-                commentStyle = { italic = true },
-                functionStyle = { italic = false },
-                keywordStyle = { italic = false, bold = false },
-                statementStyle = { italic = false, bold = false },
-                typeStyle = { italic = false },
-            })
-        end
-    },
-    {
         "folke/tokyonight.nvim",
         -- lazy = false,
         lazy=true,
