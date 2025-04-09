4bd52d0206e769ee537311f65d7b9f66dfb7df46
Daniel M Sussman <dmsussman@gmail.com>
2025-04-08 21:04:39 -0400

    zenbones

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,12 +1,12 @@
 --For colorschemes that I am not using right now
 return {
-    {
-        "folke/tokyonight.nvim",
-        -- lazy = false,
-        lazy=true,
-        -- priority = 1000,
-        opts = {},
-    },
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     -- lazy = false,
+    --     lazy=true,
+    --     -- priority = 1000,
+    --     opts = {},
+    -- },
     {
         'sainnhe/everforest',
         -- lazy = false,
@@ -20,34 +20,48 @@ return {
 --            vim.cmd.colorscheme('everforest')
         end
     },
+    -- {
+    --     "AlexvZyl/nordic.nvim",
+    --     lazy = true,
+    --   -- lazy = false,
+    --   -- priority = 1000,
+    --     config = function()
+    --         require("nordic").setup({})
+    --   --      vim.cmd.colorscheme("nord")
+    --     end,
+    -- },
     {
-        "AlexvZyl/nordic.nvim",
-        lazy = true,
-      -- lazy = false,
-      -- priority = 1000,
-        config = function()
-            require("nordic").setup({})
-      --      vim.cmd.colorscheme("nord")
-        end,
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = true,
+    priority = 1000,
+    -- you can set set configuration options here
+    -- config = function()
+    --     vim.g.zenbones_darken_comments = 45
+    --     vim.cmd.colorscheme('zenbones')
+    -- end
     },
-    {
-        "EdenEast/nightfox.nvim",
-        lazy = true,
-      -- lazy = false,
-      -- priority = 1000,
-        config = function()
-            -- require("nordic").setup({})
-      --      vim.cmd.colorscheme("nord")
-        end,
-    },
-    {
-        "sainnhe/gruvbox-material",
-        lazy = true,
-      -- lazy = false,
-      -- priority = 1000,
-        config = function()
-            -- require("nordic").setup({})
-      --      vim.cmd.colorscheme("nord")
-        end,
-    },
+    -- {
+    --     "EdenEast/nightfox.nvim",
+    --     lazy = true,
+    --   -- lazy = false,
+    --   -- priority = 1000,
+    --     config = function()
+    --         -- require("nordic").setup({})
+    --   --      vim.cmd.colorscheme("nord")
+    --     end,
+    -- },
+    -- {
+    --     "sainnhe/gruvbox-material",
+    --     lazy = true,
+    --   -- lazy = false,
+    --   -- priority = 1000,
+    --     config = function()
+    --         -- require("nordic").setup({})
+    --   --      vim.cmd.colorscheme("nord")
+    --     end,
+    -- },
 }
