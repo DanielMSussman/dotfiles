6b8eefeef95eac9990b5c0b7bd5bd2abc1749a8a
Daniel M Sussman <dmsussman@gmail.com>
2025-05-19 08:21:20 -0400

    neogen

---
+++ nvim/lua/plugins/comments.lua
@@ -0,0 +1,13 @@
+return {
+
+    {
+    "danymat/neogen",
+    ft = {"cpp","julia"},
+    config = function()
+            require('neogen').setup({
+            })
+        end,
+    -- Uncomment next line if you want to follow only stable versions
+    -- version = "*" 
+},
+}
