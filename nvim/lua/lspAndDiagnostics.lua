44aff18f6a691e8ac6fa9b2465c28c758c42beec
Daniel M Sussman <dmsussman@gmail.com>
2025-07-29 12:00:10 -0400

    lsp borders

--- nvim/lua/lspAndDiagnostics.lua
+++ nvim/lua/lspAndDiagnostics.lua
@@ -13,6 +13,15 @@ vim.diagnostic.config({
   },
 })
 
+--lsp windows get a nice little border
+local original_open_floating_preview = vim.lsp.util.open_floating_preview
+vim.lsp.util.open_floating_preview = function(contents, syntax, opts)
+  opts = opts or {}
+  opts.border = opts.border or "rounded"
+
+  return original_open_floating_preview(contents, syntax, opts)
+end
+
 -- vim.diagnostic.config({
 --   -- Use the default configuration
 --   -- virtual_lines = true
