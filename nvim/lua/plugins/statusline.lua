e9dcc332f9cbcf5b655f1479da26685788169f49
Daniel M Sussman <dmsussman@gmail.com>
2025-08-07 08:54:56 -0400

    statusline

---
+++ nvim/lua/plugins/statusline.lua
@@ -0,0 +1,42 @@
+return {
+ 	{ 'echasnovski/mini.statusline', version = false,
+    dependencies = { "nvim-tree/nvim-web-devicons"},
+        config = function()
+            local statusline = require('mini.statusline')
+            local fileinfo      = function(args)
+                local filetype = vim.bo.filetype
+                local devicons = require('nvim-web-devicons')
+                local icon = devicons.get_icon(vim.fn.expand('%:t'), nil, { default = true }) .. ' '
+                return string.format('%s%s', icon, filetype)
+            end
+
+            local contents = function()
+                local mode, mode_hl = statusline.section_mode({ trunc_width = 80 })
+                local diagnostics   = statusline.section_diagnostics({ trunc_width = 75 })
+                local lsp           = statusline.section_lsp({ trunc_width = 75 })
+                local filename      = statusline.section_filename({ trunc_width = 140 })
+                -- local fileinfo      = statusline.section_fileinfo({ trunc_width = 80 })
+                -- local location      = statusline.section_location({ trunc_width = 75 })
+                --                       '%l|%L|%2v|%-2{virtcol("$") -1}'   
+                local location = '%l,%2v'
+                local search        = statusline.section_searchcount({ trunc_width = 75 })
+                local infostring = fileinfo()
+                -- print(vim.inspect(infostring))
+                return statusline.combine_groups({
+                    { hl = mode_hl,                  strings = { mode } },
+                    { hl = 'MiniStatuslineDevinfo',  strings = { diagnostics, lsp } },
+                    '%<', -- Mark general truncate point
+                    { hl = 'MiniStatuslineFilename', strings = { filename } },
+                    '%=', -- End left alignment
+                    { hl = 'MiniStatuslineFileinfo', strings = { fileinfo() } },
+                    { hl = mode_hl,                  strings = { search, location } },
+                })
+            end
+
+
+            statusline.setup({
+                content = {active = contents},
+            })
+        end
+    },
+}
