4381a0d1ec8628d0cca173579e05f2abb921cf90
Daniel M Sussman <dmsussman@gmail.com>
2025-11-01 21:58:11 -0400

    testing wsl clipboard alternative

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,6 +39,8 @@ vim.keymap.set("n", "<leader>t",
 
 -- potential flags for some platform-specific options in plugins
 isWindows = vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 or vim.fn.has("win16") == 1
+isWSL = vim.fn.has("wsl")
+
 --recommended settings to use pwsh instead of cmd.exe
 if isWindows then
     vim.o.shell="pwsh"
@@ -47,4 +49,25 @@ if isWindows then
     vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
     vim.o.shellquote = ""
     vim.o.shellxquote = ""
+end
+
+if isWSL then
+    if vim.fn.executable('wl-copy') == 1 then
+        vim.g.clipboard = {
+            name = 'wl-clipboard',
+            copy = {
+                ["+"] = "wl-copy --foreground --type text/plain",
+                ["*"] = "wl-copy --foreground --primary --type text/plain",
+            },
+            paste = {
+                ["+"] = function()
+                    return vim.fn.systemlist('wl-paste --no-newline|sed -e "s/\r$//"', { "" }, 1) -- '1' keeps empty lines
+                end,
+                ["*"] = function()
+                    return vim.fn.systemlist('wl-paste --primary --no-newline|sed -e "s/\r$//"', { "" }, 1)
+                end,
+            },
+            cache_enabled = 1,
+        }
+    end
 end
